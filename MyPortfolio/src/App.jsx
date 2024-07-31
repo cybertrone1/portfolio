a39dbb5c44c7acc8bf5b2a915d5f26b252c86c43
Hamzat Abdulaleem <hamzatabdulaleem123@gmail.com>
2024-07-31 13:43:30 -0700

    Add MyPortfolio project

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Footer from './Footer';
+import About from './About';
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
         <div className="sub-content">
           <Routes>
             <Route exact path='/' element={ < Home /> } />
-
+            <Route exact path='/about' element={ < About /> } />
           </Routes>
         </div>
         < Footer />
